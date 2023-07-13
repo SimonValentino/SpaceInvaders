d27dc043afd933cd15285b00e25e2e62a6d9cdb7
simonvalentino <simontvalentino@gmail.com>
2023-07-13 17:00:39 -0400

    Created loop to run game and a blank screen.

--- runner.py
+++ runner.py
@@ -1,3 +1,12 @@
 import pygame
 
+
 pygame.init()
+
+screen = pygame.display.set_mode((800, 800))
+
+run_game = True
+while run_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run_game = False
