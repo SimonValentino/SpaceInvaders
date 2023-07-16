7a3d87ec629866af868382b878eb8707db444094
simonvalentino <simontvalentino@gmail.com>
2023-07-16 16:42:53 -0400

    Moving defender.

--- constants.py
+++ constants.py
@@ -5,3 +5,8 @@ INITIAL_PLAYER_COORDINATES = (INITIAL_SCREEN_SIZE[0] / 2 - ICON_SIZE[0] / 2,
                               INITIAL_SCREEN_SIZE[1] / 6 * 5)
 
 INITIAL_SCREEN_COLOR = (0, 0, 0)
+
+# Num pixels the player moves left or right with each key pressed
+PLAYER_SPEED = 0.5
+
+FPS = 30.0
--- runner.py
+++ runner.py
@@ -1,14 +1,12 @@
 import pygame
 import constants as consts
 
-
-def draw_player():
-    screen.blit(defender, (player_x, player_y))
-
+def draw_player(x, y):
+    screen.blit(defender, (x, y))
 
 pygame.init()
 
-screen = pygame.display.set_mode(consts.INITIAL_SCREEN_SIZE, pygame.RESIZABLE)
+screen = pygame.display.set_mode(consts.INITIAL_SCREEN_SIZE)
 
 # Initialize the display
 pygame.display.set_caption("Space Invaders")
@@ -18,6 +16,10 @@ pygame.display.set_icon(pygame.image.load("assets/game_logo.png"))
 defender = pygame.image.load("assets/defender.png")
 player_x, player_y = consts.INITIAL_PLAYER_COORDINATES
 
+# Define movement flags
+move_left = False
+move_right = False
+
 run_game = True
 while run_game:
     screen.fill(consts.INITIAL_SCREEN_COLOR)
@@ -26,5 +28,25 @@ while run_game:
         if event.type == pygame.QUIT:
             run_game = False
 
-    draw_player()
+        # Key is pressed down
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                move_right = True
+            elif event.key == pygame.K_LEFT:
+                move_left = True
+
+        # Key is released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                move_right = False
+            elif event.key == pygame.K_LEFT:
+                move_left = False
+
+    # Update player position based on movement flags
+    if move_right:
+        player_x += consts.PLAYER_SPEED
+    if move_left:
+        player_x -= consts.PLAYER_SPEED
+
+    draw_player(player_x, player_y)
     pygame.display.update()
